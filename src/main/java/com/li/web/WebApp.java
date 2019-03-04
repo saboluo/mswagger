04cb89b6067197ee6221ce2693e00dc7eaecdef3
saboluo <397142058@qq.com>
2019-03-04 23:18:52 +0800

    cc

---
+++ src/main/java/com/li/web/WebApp.java
@@ -0,0 +1,10 @@
+package com.li.web;
+
+/**
+ * Created by li on 2019/3/4.
+ */
+public class WebApp {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
